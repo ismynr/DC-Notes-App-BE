23d3be6e695bf0b5195a7fd37aae5b6dcb058aac
ismynr <gekkai7is.my@gmail.com>
2022-04-16 06:44:18 +0700

    collaboration implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -64,6 +64,16 @@ class UsersService {
     }
     return id;
   }
+
+  async getUsersByUsername(username) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
+      values: [`%${username}%`],
+    };
+    console.log(query);
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
 }
 
 module.exports = UsersService;
